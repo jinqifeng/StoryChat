6af0e281c11c19a13ab05144ee4e079d7d221f37
DESKTOP-SKRF3M1\JongWN-D <jinqifeng@mail.com>
2017-08-23 14:15:44 +0800

    no message

--- StoryChat2/app/src/main/java/com/jwn/storychat/login.java
+++ StoryChat2/app/src/main/java/com/jwn/storychat/login.java
@@ -1,8 +1,19 @@
 package com.jwn.storychat;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by JongWN-D on 8/23/2017.
  */
 
-public class login {
+public class login extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
+    }
 }
--- StoryChat2/app/src/main/java/com/jwn/storychat/register.java
+++ StoryChat2/app/src/main/java/com/jwn/storychat/register.java
@@ -1,8 +1,19 @@
 package com.jwn.storychat;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by JongWN-D on 8/23/2017.
  */
 
-public class register {
+public class register extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
+    }
 }
